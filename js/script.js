bfc696728f18ac6af57b1571d2043806a2e51153
Boris Kim <40835487+kibo13@users.noreply.github.com>
2019-06-23 15:58:03 +0500

    change modal window for section portfolio

--- js/script.js
+++ js/script.js
@@ -42,43 +42,43 @@ $(document).ready(function () {
   /* control active link on scroll */
   /* ============================= */
 
-  var lastId,
-    topMenu = $(".navbar__list"),
-    topMenuHeight = topMenu.outerHeight() + 15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function () {
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
+  // var lastId,
+  //   topMenu = $(".navbar__list"),
+  //   topMenuHeight = topMenu.outerHeight() + 15,
+  //   // All list items
+  //   menuItems = topMenu.find("a"),
+  //   // Anchors corresponding to menu items
+  //   scrollItems = menuItems.map(function () {
+  //     var item = $($(this).attr("href"));
+  //     if (item.length) { return item; }
+  //   });
 
-  menuItems.click(function (e) {
-    var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top;
-    $('html, body').stop().animate({
-      scrollTop: offsetTop 
-    }, 1500);
-    e.preventDefault();
-  });
+  // menuItems.click(function (e) {
+  //   var href = $(this).attr("href"),
+  //     offsetTop = href === "#" ? 0 : $(href).offset().top;
+  //   $('html, body').stop().animate({
+  //     scrollTop: offsetTop 
+  //   }, 1500);
+  //   e.preventDefault();
+  // });
 
-  $(window).scroll(function () {
-    var fromTop = $(this).scrollTop() + topMenuHeight;
-    var cur = scrollItems.map(function () {
-      if ($(this).offset().top < fromTop)
-        return this;
-    });
+  // $(window).scroll(function () {
+  //   var fromTop = $(this).scrollTop() + topMenuHeight;
+  //   var cur = scrollItems.map(function () {
+  //     if ($(this).offset().top < fromTop)
+  //       return this;
+  //   });
 
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
+  //   cur = cur[cur.length - 1];
+  //   var id = cur && cur.length ? cur[0].id : "";
 
-    if (lastId !== id) {
-      lastId = id;
-      menuItems
-        .parent().removeClass("active")
-        .end().filter("[href='#" + id + "']").parent().addClass("active");
-    }
-  });
+  //   if (lastId !== id) {
+  //     lastId = id;
+  //     menuItems
+  //       .parent().removeClass("active")
+  //       .end().filter("[href='#" + id + "']").parent().addClass("active");
+  //   }
+  // });
 
   /* ================================= */
   /* options for toggler block of test */
@@ -111,11 +111,11 @@ $(document).ready(function () {
 
 window.onload = function () {
 
+  const BODY = document.querySelector('body');
+
   /* ============ */
   /*   preloader  */
-  /* ============ */
-
-  const BODY      = document.querySelector('body');
+  /* ============ */ 
 
   setTimeout(function () {
     let preloader = document.getElementById('preloader');
@@ -129,47 +129,53 @@ window.onload = function () {
   /* ========= */
 
   var mModal  = document.getElementById('modal'),
-      mClose  = document.getElementById('close'),
-      mTitle  = document.getElementById('title'),
-      mLink   = document.getElementById('link'),
-      mAim    = document.getElementById('aim'),
-      mToDo   = document.getElementById('todo'),
-      mTool   = document.getElementById('tool'),
-      mResult = document.getElementById('result'),
+      mInput  = document.getElementById('m-input'),
+      mImg    = document.getElementById('m-img'),
+      mClose  = document.getElementById('m-close'),
+      mTitle  = document.getElementById('m-title'),
+      mAim    = document.getElementById('m-aim'),
+      mToDo   = document.getElementById('m-todo'),
+      mTool   = document.getElementById('m-tool'),
+      mResult = document.getElementById('m-result'),
+      mLink   = document.getElementById('m-link'),
       storage = JSON.parse(`
         {
           "data": {
 						"1": {
-							"title": "Фабрика скинали",
-							"link": "https://kibo13.github.io/skinali/",
-							"aim": "разработать адаптивный сайт с использованием методологии БЭМ.",
-							"todo": "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер, адаптивный таймлайн и плавная прокрутка целевой страницы. Выполнена оптимизация файлов(изображений, js, css) для уменьшения времени загрузки сайта.",
-							"tool": "HTML, CSS, SASS, jQuery, JavaScript, GitHub, Photoshop, Yandex Map API, Google PageSpeed Insights.",
-							"result": "освоил разработку сайта по методологии БЭМ. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов. При работе над проектом активно использовалась система контроля версий Git."
+              "title"  : "Фабрика скинали",
+              "img"    : "img/modal/modal-04.jpg",
+							"link"   : "https://kibo13.github.io/skinali/",
+							"aim"    : "разработать адаптивный сайт с использованием методологии БЭМ.",
+							"todo"   : "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер, адаптивный таймлайн и плавная прокрутка целевой страницы. Выполнена оптимизация файлов(изображений, js, css) для уменьшения времени загрузки сайта.",
+							"tool"   : "HTML, CSS, SASS, jQuery, JavaScript, GitHub, Photoshop, Yandex Map API, Google PageSpeed Insights.",
+							"result" : "освоил разработку сайта по методологии БЭМ. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов. При работе над проектом активно использовалась система контроля версий Git."
 						},
 						"2": {
-							"title": "Advoagency",
-							"link": "https://kibo13.github.io/advoagency/",
-							"aim": "разработать адаптивный сайт с использованием методологии БЭМ.",
-							"todo": "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер и плавная прокрутка целевой страницы. Выполнена оптимизация файлов(изображений, js, css) для уменьшения времени загрузки сайта.",
-							"tool": "HTML, CSS, SASS, JavaScript, jQuery, Photoshop, GitHub, Google PageSpeed Insights.",
-							"result": "освоил разработку сайта по методологии БЭМ. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов. При работе над проектом активно использовалась система контроля версий Git."
+              "title"  : "Advoagency",
+              "img"    : "img/modal/modal-03.jpg",
+							"link"   : "https://kibo13.github.io/advoagency/",
+							"aim"    : "разработать адаптивный сайт с использованием методологии БЭМ.",
+							"todo"   : "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер и плавная прокрутка целевой страницы. Выполнена оптимизация файлов(изображений, js, css) для уменьшения времени загрузки сайта.",
+							"tool"   : "HTML, CSS, SASS, JavaScript, jQuery, Photoshop, GitHub, Google PageSpeed Insights.",
+							"result" : "освоил разработку сайта по методологии БЭМ. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов. При работе над проектом активно использовалась система контроля версий Git."
 						},
 						"3": {
-							"title": "Degcompany",
-							"link": "https://kibo13.github.io/degcompany/",
-							"aim": "разработать адаптивный сайт с применением технологии Flexbox и Grid.",
-							"todo": "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер, плавная прокрутка целевой страницы и форма обратной связи с проверкой на корректность вводимых данных (валидация формы).",
-							"tool": "HTML, CSS, SCSS, Koala, JavaScript, jQuery, GitHub",
-							"result": "освоил разработку сайта c применением технологии Flexbox и Grid. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов."
+              "title"  : "Degcompany",
+              "img"    : "img/modal/modal-01.jpg",
+							"link"   : "https://kibo13.github.io/degcompany/",
+							"aim"    : "разработать адаптивный сайт с применением технологии Flexbox и Grid.",
+							"todo"   : "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер, плавная прокрутка целевой страницы и форма обратной связи с проверкой на корректность вводимых данных (валидация формы).",
+							"tool"   : "HTML, CSS, SCSS, Koala, JavaScript, jQuery, GitHub",
+							"result" : "освоил разработку сайта c применением технологии Flexbox и Grid. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов."
 						},
 						"4": {
-							"title": "Skyup",
-							"link": "https://kibo13.github.io/skyup/",
-							"aim": "разработать адаптивный сайт с использованием сетки Bootstrap.",
-							"todo": "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер, параллакс эффект и плавная прокрутка целевой страницы.",
-							"tool": "Bootstrap, HTML, CSS, SCSS, Koala, JavaScript, jQuery, GitHub, Google Map API.",
-							"result": "освоил разработку сайта c применением инструментов Bootstrap. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов."
+              "title"  : "Skyup",
+              "img"    : "img/modal/modal-02.jpg",
+							"link"   : "https://kibo13.github.io/skyup/",
+							"aim"    : "разработать адаптивный сайт с использованием сетки Bootstrap.",
+							"todo"   : "разработан адаптивный сайт. Добавлены CSS-анимации (прелоадер, ссылки, меню, кнопки), а также слайдер, параллакс эффект и плавная прокрутка целевой страницы.",
+							"tool"   : "Bootstrap, HTML, CSS, SCSS, Koala, JavaScript, jQuery, GitHub, Google Map API.",
+							"result" : "освоил разработку сайта c применением инструментов Bootstrap. Более детально изучил использование JavaScript/JQuery для создания интерактивных элементов."
 						}
           }
         }
@@ -183,14 +189,17 @@ window.onload = function () {
   }
 
   function createWindow(data, id) {
-    mTitle.innerHTML = data.title;
-    mLink.href = data.link;
-    mAim.innerHTML = data.aim;
-    mToDo.innerHTML = data.todo;
-    mTool.innerHTML = data.tool;
-    mResult.innerHTML = data.result;
-    mModal.style.display = 'flex';
-    BODY.style.overflow = 'hidden';
+    mTitle.innerHTML      = data.title;
+    mLink.href            = data.link;
+    mLink.innerHTML       = data.link;
+    mInput.value          = data.link;
+    mImg.src              = data.img;
+    mAim.innerHTML        = data.aim;
+    mToDo.innerHTML       = data.todo;
+    mTool.innerHTML       = data.tool;
+    mResult.innerHTML     = data.result;
+    mModal.style.display  = 'flex';
+    BODY.style.overflow   = 'hidden';
   }
 
   document.querySelector('.portfolio').addEventListener('click', function (e) {
